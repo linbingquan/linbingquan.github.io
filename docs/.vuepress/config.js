77fbaa89fa842545300f8996cc47bd61c8fd711a
linbingquan <695601626@qq.com>
2019-01-24 23:57:43 +0800

    :sunny: 我的博客

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: '林炳权的小博', // 设置网站标题
-  description: '林炳权的小博', // 设置网站描述
+  title: '林炳权的博客', // 设置网站标题
+  description: '林炳权的博客', // 设置网站描述
   base: '/',
   themeConfig: {
     sidebar: [
